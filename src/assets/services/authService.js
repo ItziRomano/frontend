42d45be5ed8c5d1e341b097514d50923dab457cd
ItziRomano <itzi.romanolo@gmail.com>
2025-05-22 23:08:14 -0600

    cambio de api url

--- src/assets/services/authService.js
+++ src/assets/services/authService.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:5000/api/v1/auth'
+// const API_URL = 'http://localhost:5000/api/v1/auth'
+const API_URL = `${import.meta.env.VITE_API_URL}/auth`
 
 const register = async (email, password, firstName, lastName) => {
   const response = await axios.post(`${API_URL}/register`, {
